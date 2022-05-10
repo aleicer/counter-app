bcd8a0d50c7e8b31dda109e1b86c68717df56127
Aleicer AV. Vesga Rueda <avesga@emi.med>
2022-05-09 21:07:54 -0500

    primer commit, iniciando en react

---
+++ src/PrimeraApp.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react'
+
+const PrimeraApp = () => {
+
+    const saludo = 'Hola Mundo'
+    
+    return (
+        <>
+            <h1>{ saludo }</h1>
+            <p>Mi primera app</p>
+        </>
+    )
+}
+
+export default PrimeraApp;
